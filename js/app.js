a84d42150ed4c6f5b79667eca0aeae9d8e3e6835
claytonschoonoversmith@gmail.com <claytonsmith@Claytons-MacBook-Pro.local>
2019-04-01 20:15:11 -0500

    added js logic new avatar images

--- js/app.js
+++ js/app.js
@@ -2,21 +2,35 @@ console.log('CREATE A TAMAGOTCHI!');
 console.log("I'm an elf!");
 
 //make a class which will create a tamagotchi class instance
-class Tamagotchi {
+class Pet {
 	constructor() {
-		// this.boredom = ''; scale of 1-10
-		// this.hunger = ''; scale of 1-10
-		// this.sleepiness = ''; scale of 1-10
-
+		this.boredom = 1;
+		this.sleepiness = 1;
+		this.boredom = 1;
+		this.age = 1;
+		this.lightsOn = true;
+		this.isAlive = true;
 	}
 }
 
+const tamagotchi = new Pet();
 
 
 
 
 
 
+
+
+const updatePet = () => {
+	while (Pet.isAlive = true){
+		
+	}
+	// console.log(Pet.boredom);
+	// console.log(Pet.sleepiness);
+	// console.log(Pet.boredom);
+	// console.log(Pet.age);
+}
 
 
 
@@ -46,13 +60,6 @@ class Tamagotchi {
 //make and object to put all your stuff into - sleepy, hungry, timer, methods(), etc. 
 //don't try to write logic here!  Don't try to write methods in the global scale - too
 //hard to rewrite things after declared in the global scale.
-const app = {
-	update_pet(){
-		boredom: "";
-		hunger: "";
-		sleepiness: "";
-	}
-}
 
 
 
@@ -90,47 +97,18 @@ const app = {
 //Listeners / Handlers
 $("#feed").on('click', (e) => {
 	console.log('feed button was clicked');
+	app.hunger -= 3;
 });
 
 $("#turnOffLights").on('click', (e) => {
 	console.log('turnOffLights button was clicked');
+	app.lightsOn = false;
+	app.sleepiness -= 3;
 });
 
 $("#play").on('click', (e) => {
 	console.log('play button was clicked');
+	app.boredom -= 3;
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
