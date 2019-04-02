0ec2f982323f9b48555f747c2c0e54dcef2d7192
claytonschoonoversmith@gmail.com <claytonsmith@Claytons-MacBook-Pro.local>
2019-04-01 19:06:05 -0500

    added more styling, began js classes/app obj/listeners

--- js/app.js
+++ js/app.js
@@ -4,9 +4,10 @@ console.log("I'm an elf!");
 //make a class which will create a tamagotchi class instance
 class Tamagotchi {
 	constructor() {
-		this.boredom = ''; //scale of 1-10
-		this.hunger = ''; //scale of 1-10
-		this.sleepiness = ''; //scale of 1-10
+		// this.boredom = ''; scale of 1-10
+		// this.hunger = ''; scale of 1-10
+		// this.sleepiness = ''; scale of 1-10
+
 	}
 }
 
@@ -45,7 +46,13 @@ class Tamagotchi {
 //make and object to put all your stuff into - sleepy, hungry, timer, methods(), etc. 
 //don't try to write logic here!  Don't try to write methods in the global scale - too
 //hard to rewrite things after declared in the global scale.
-
+const app = {
+	update_pet(){
+		boredom: "";
+		hunger: "";
+		sleepiness: "";
+	}
+}
 
 
 
@@ -82,15 +89,15 @@ class Tamagotchi {
 
 //Listeners / Handlers
 $("#feed").on('click', (e) => {
-	console.log('feed button was clicked')
+	console.log('feed button was clicked');
 });
 
 $("#turnOffLights").on('click', (e) => {
-	console.log('turnOffLights button was clicked')
+	console.log('turnOffLights button was clicked');
 });
 
 $("#play").on('click', (e) => {
-	console.log('play button was clicked')
+	console.log('play button was clicked');
 });
 
 
