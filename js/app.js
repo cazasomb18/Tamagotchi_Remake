86721c2ee7970b0577c0ed06f0dd9a8997afaf01
claytonschoonoversmith@gmail.com <claytonsmith@Claytons-MacBook-Pro.local>
2019-04-02 22:22:39 -0500

    working on getting screen to update metrics

--- js/app.js
+++ js/app.js
@@ -24,50 +24,88 @@ class Pet {
 //hard to rewrite things after declared in the global scale.
 
 const App = {
-	timerHandle: 0,
-	toggleLights: true,
-	petInstance: null,
-	playGame: function(){
-		const tamagotchi = new Pet('Klyza');
-		this.petInstance = tamagotchi;
-		setInterval (function() {
-			console.log(App.petInstance)
-			App.hungerIncrement();
-			App.boredomIncrement();
-			App.sleepinessIncrement();
-			App.ageIncrement();
-			App.deathFunction();
-			App.timerHandle += 1;
-		}, 300);
-	},
-	ageIncrement: function(){
-		if (this.timerHandle % 10 === 0){
-			// console.log(this.petInstance.age);
-			this.petInstance.age ++;
-		}
-	},
-	hungerIncrement: function(){	
-		if (this.timerHandle % 3 === 0){
-			// console.log(this.petInstance.hunger);
-			this.petInstance.hunger ++;
-		}
-	},
-	boredomIncrement: function(){
-		if (this.timerHandle % 5 === 0){
-			// console.log(this.petInstance.boredom);
-			this.petInstance.boredom ++;
-		}
-	},
-	sleepinessIncrement: function(){
-		if (this.timerHandle % 7 === 0){
-			// console.log(this.petInstance.sleepiness);
-			this.petInstance.sleepiness ++;
-		}
-	},
-	deathFunction: function(){
-		if (this.petInstance.hunger >= 10 || this.petInstance.boredom >= 10 || this.petInstance.sleepiness >=10){
-			this.petInstance = null;
-		}
+        timerHandle: 0,
+        toggleLights: true,
+        petInstance: null,
+        playGame: function() {
+            const tamagotchi = new Pet('Klyza');
+            this.petInstance = tamagotchi;
+            setInterval(function() {
+                console.log(App.petInstance)
+                App.hungerIncrement();
+                App.boredomIncrement();
+                App.sleepinessIncrement();
+                App.ageIncrement();
+                App.deathFunction();
+                App.timerHandle += 1;
+            }, 300);
+        },
+        ageIncrement: function() {
+            if (this.timerHandle % 10 === 0) {
+                // console.log(this.petInstance.age);
+                this.petInstance.age++;
+            }
+        },
+        hungerIncrement: function() {
+            if (this.timerHandle % 3 === 0) {
+                // console.log(this.petInstance.hunger);
+                this.petInstance.hunger++;
+            }
+        },
+        boredomIncrement: function() {
+            if (this.timerHandle % 5 === 0) {
+                // console.log(this.petInstance.boredom);
+                this.petInstance.boredom++;
+            }
+        },
+        sleepinessIncrement: function() {
+            if (this.timerHandle % 7 === 0) {
+                // console.log(this.petInstance.sleepiness);
+                this.petInstance.sleepiness++;
+            }
+        },
+        deathFunction: function() {
+                if (this.petInstance.hunger >= 10 || this.petInstance.boredom >= 10 || 
+                	this.petInstance.sleepiness >= 10) {
+                    this.petInstance = null;
+                }
+
+
+$("#feed").on('click', (e) => {
+    console.log('feed button was clicked');
+    App.petInstance.hunger -= 1;
+    console.log(`Hunger is now: ${App.petInstance.hunger}`);
+});
+
+$("#toggleLights").on('click', (e) => {
+    console.log('toggleLights button was clicked');
+    App.petInstance.sleepiness -=1; //
+    $("#screenContainer").css("backgroundColor", 'rgba(0,0,0,.7)');
+    App.toggleLights === false; //===
+    if (App.toggleLights === false) { //===
+        App.toggleLights = true; //=
+        App.petInstance.sleepiness += 1; //-=1
+    } else {
+       App.toggleLights = false; //false
+       $("#screenContainer").css("backgroundColor", 'rgba(0,0,0,1)');
+    }
+
+});
+
+$("#play").on('click', (e) => {
+    console.log('play button was clicked');
+    App.petInstance.boredom -= 1;
+    console.log(`Boredom is now: ${App.petInstance.boredom}`);
+})
+}
+}
+
+// $('#begin-btn').on('click', () => {
+//     const tamaName = $('#input-box').val();
+//     $('h2').text(`${tamaName} is ALIVE! Keep it that way!!`); 
+//     game.startTimer()
+// });
+
 
 //timer: set interval - within set interval have it do other things:
 //will have timer after every time is goes through will call bunch of functions
@@ -77,33 +115,7 @@ const App = {
 
 //Listeners / Handlers
 
-$("#feed").on('click', (e) => {
-	console.log('feed button was clicked');
-	App.petInstance.hunger -= 1;
-	console.log(`Hunger is now: ${App.petInstance.hunger}`);
-});
 
-$("#toggleLights").on('click', (e) => {
-    console.log('toggleLights button was clicked');
-    // App.petInstance.sleepiness -=1;
-    $("#screenContainer").css("backgroundColor", 'rgba(0,0,0,.7)');
-    App.toggleLights === false; //===
-    if (App.toggleLights === false) { //===
-        App.toggleLights = true; //=
-        App.petInstance.sleepiness -= 1;
-    } else {
-        App.toggleLights = false; //=
-        $("#screenContainer").css("backgroundColor", 'rgba(0,0,0)');
-    }
-   	
-});
-
-$("#play").on('click', (e) => {
-	console.log('play button was clicked');
-	App.petInstance.boredom -= 1;
-	console.log(`Boredom is now: ${App.petInstance.boredom}`);
-});
-}}
 
 
 // $("#toggleLights").on(function(e) {
