e16953500a20afec758ebb0910736778d58a5f00
claytonschoonoversmith@gmail.com <claytonsmith@Claytons-MacBook-Pro.local>
2019-04-03 16:07:54 -0500

    still couldn't get the images to change at set interval

--- js/app.js
+++ js/app.js
@@ -24,100 +24,95 @@ class Pet {
 
 
 
+
 const App = {
-        timerHandle: 0,
-        toggleLights: true,
-        petInstance: null,
-        playGame: function() {
-            const tamagotchi = new Pet($('#nameBox').val());
-            this.petInstance = tamagotchi;
-            setInterval(function() {
-                App.ageIncrement();
-                App.hungerIncrement(); 
-                App.boredomIncrement(); 
-                App.sleepinessIncrement(); 
-                App.timerHandle += 1;
-                App.deathFunction();
-                App.displayName();
-                App.displayHunger();
-                App.displaySleepiness();
-                App.displayAge();
-                App.displayBoredom();
-                App.morphAvatar();
-                App.animateAvatar();
-                console.log(App.petInstance);
-            }, 300)
-            if (this.petInstance.isAlive = false){
-            	clearInterval(deathFunction());
-            }
-        },
-        ageIncrement: function() {
-            if (this.timerHandle % 10 === 0) {  //this
-                console.log(this.petInstance.age);
-                this.petInstance.age += 1;
-            }
-        },
-        hungerIncrement: function() {
-            if (this.timerHandle % 3 === 0) { //this
-                console.log(this.petInstance.hunger);
-                this.petInstance.hunger += 1;
-            }
-        },
-        boredomIncrement: function() {  //this
-            if (this.timerHandle % 5 === 0) {
-                console.log(this.petInstance.boredom);
-                this.petInstance.boredom += 1;
-            }
-        },
-        sleepinessIncrement: function() {
-            if (this.timerHandle % 7 === 0) {
-                console.log(this.petInstance.sleepiness);
-                this.petInstance.sleepiness += 1;
-            }
-        },
-        deathFunction: function() {
-                if (this.petInstance.hunger >= 10 || this.petInstance.boredom >= 10 || 
-                	this.petInstance = null || this.petInstance.sleepiness >= 10 || 
-                	App.isAlive === false;) {
-                    console.log('YOU ARE A TERRIBLE PARENT AND YOUR PET HAS DIED!!!');
-                    App.
-                }
-			},
-		displayName: function (){
-			const $petName = $('#nameBox').val();
-			$('#nameDisplay').text(`${this.petInstance.name}`).val();
-			},
-		displayHunger: function(){
-			$('#hungerOutput').text(`${this.petInstance.hunger}`);
-			$('#hungerOutput').val('');
-			},
-		displaySleepiness: function(){
-			$('#sleepinessOutput').text(`${this.petInstance.sleepiness}`);
-			$('#sleepinessOutput').val('');
-		},
-		displayAge: function(){
-			$('#ageOutput').text(`${this.petInstance.age}`);
-		},
-		displayBoredom: function(){
-			$('#boredomOutput').text(`${this.petInstance.boredom}`);
-			$('#boredomOutput').val('');
-		},
-		morphAvatar: function (){
-			if (this.petInstance.age % 5 === 0){
-				let a = Math.random();
-				$("#tamagotchiAvatar").css("opacity", `${a}`);
-				} else ($("#tamagotchiAvatar").css("opacity", '1'));
-			},
-		animateAvatar: function(){
-			if (this.timerHandle % 2 === 0){
-				$('#image').css("padding-left", 50)
-				$('#image').css("padding-right", 0)
+    timerHandle: 0,
+    toggleLights: true,
+    petInstance: null,
+    playGame: function() {
+        const tamagotchi = new Pet($('#nameBox').val());
+        this.petInstance = tamagotchi;
+        setInterval(function() {
+            App.ageIncrement();
+            App.hungerIncrement();
+            App.boredomIncrement();
+            App.sleepinessIncrement();
+            App.timerHandle += 1;
+            App.deathFunction();
+            App.displayName();
+            App.displayHunger();
+            App.displaySleepiness();
+            App.displayAge();
+            App.displayBoredom();
+            App.morphAvatar();
+            App.animateAvatar();
+            console.log(App.petInstance);
+        }, 300)
+        if (this.petInstance.isAlive = false) {
+            clearInterval(deathFunction());
+        }
+    },
+    ageIncrement: function() {
+        if (this.timerHandle % 10 === 0) { //this
+            console.log(this.petInstance.age);
+            this.petInstance.age += 1;
+        }
+    },
+    hungerIncrement: function() {
+        if (this.timerHandle % 3 === 0) { //this
+            console.log(this.petInstance.hunger);
+            this.petInstance.hunger += 1;
+        }
+    },
+    boredomIncrement: function() { //this
+        if (this.timerHandle % 5 === 0) {
+            console.log(this.petInstance.boredom);
+            this.petInstance.boredom += 1;
+        }
+    },
+    sleepinessIncrement: function() {
+        if (this.timerHandle % 7 === 0) {
+            console.log(this.petInstance.sleepiness);
+            this.petInstance.sleepiness += 1;
+        }
+    },
+    deathFunction: function() {
+        if (this.petInstance.hunger >= 10 || this.petInstance.boredom >= 10 ||
+            this.petInstance === null || this.petInstance.sleepiness >= 10 ||
+            this.isAlive === false) {
+            this.App = null;
+            this.setInterval = null;
+            this.timerHandle = 0;
 
-			} else if (this.timerHandle % 2 === 1)
-				$('#image').css("padding-left", 0)
-				$("#image").css("padding-right", 50);
-				} 
-		}
+            console.log('YOU ARE A TERRIBLE PARENT AND YOUR PET HAS DIED!!!');
+        }
+    },
+    displayName: function() {
+        const $petName = $('#nameBox').val();
+        $('#nameDisplay').text(`${this.petInstance.name}`).val();
+    },
+    displayHunger: function() {
+        $('#hungerOutput').text(`${this.petInstance.hunger}`);
+        $('#hungerOutput').val('');
+    },
+    displaySleepiness: function() {
+        $('#sleepinessOutput').text(`${this.petInstance.sleepiness}`);
+        $('#sleepinessOutput').val('');
+    },
+    displayAge: function() {
+        $('#ageOutput').text(`${this.petInstance.age}`);
+    },
+    displayBoredom: function() {
+        $('#boredomOutput').text(`${this.petInstance.boredom}`);
+        $('#boredomOutput').val('');
+    },
+    morphAvatar: function (){
+    	if (this.petInstance.age % 5 === 0){
+    		let a = Math.random();
+    		$("#tamagotchiAvatar").css("opacity", `${a}`);
+    		} else ($("#tamagotchiAvatar").css("opacity", '1'));
+    	},
+	};
 
 
 
@@ -131,15 +126,8 @@ const App = {
 
 
 
-		// animateAvatar: function(){
-		// 	if (this.petInstance.isAlive === true){
-		// 		('#tamagotchiAvatar').animate({ width: 100 }, "easeInSine");
-		// 	}
-		// }
 
 //Listeners / Handlers
-
-
 
 
 $("#feed").on('click', (e) => {
